6d1e6da6a9d30219e8c13206d3f6881e51b7948c
SHARMARKE NUR <Snur206@yahoo.com>
2023-01-24 15:40:03 -0800

    initial commit from Spring init

---
+++ src/main/java/com/SharmarkeNSongr/SNsongr/SNsongrApplication.java
@@ -0,0 +1,13 @@
+package com.SharmarkeNSongr.SNsongr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SNsongrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SNsongrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SharmarkeNSongr/SNsongr/SNsongrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SharmarkeNSongr.SNsongr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SNsongrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
