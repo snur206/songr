3237d2abfbb53bfa8919a77c315f553b6fd7da41
SHARMARKE NUR <Snur206@yahoo.com>
2023-01-26 16:33:30 -0800

    updated lab showing forms

--- src/main/java/com/SharmarkeNSongr/songr/Controller/albumController.java
+++ src/main/java/com/SharmarkeNSongr/songr/Controller/albumController.java
@@ -20,35 +20,27 @@ public class albumController {
     // get /albums
     @GetMapping("/albums")
     public String getAllAlbums(Model m){
-        // first, create albums
-        ArrayList<String> album = new ArrayList<>();
-        Album album1 = new Album("Beautiful Minds", "Rod Wave", 24, 68.20, "https://images.complex.com/complex/images/c_fill,dpr_2.0,f_auto,q_auto,w_1400/fl_lossy,pg_1/nx8kyc4u7hwlrfkejotl/rw?fimg-ssr");
-        Album album2 = new Album("Her Loss", "Drake & 21 Savage", 16, 60.33, "https://images.complex.com/complex/images/c_fill,dpr_2.0,f_auto,q_auto,w_1400/fl_lossy,pg_1/rrgyzeu7ucbf5styxkuw/drake-21-savage-her-loss-album-cver?fimg-ssr");
-        Album album3 = new Album("An Evening with Silk Sonic", "Silk Sonic", 10, 31.19, "https://images.complex.com/complex/images/c_fill,dpr_2.0,f_auto,q_auto,w_1400/fl_lossy,pg_1/n1spfyib0hwmvx7xvu75/silk-sonic?fimg-ssr");
+//        ArrayList<String> album = new ArrayList<>();
+//        Album album1 = new Album("Beautiful Minds", "Rod Wave", 24, 68.20, "https://images.complex.com/complex/images/c_fill,dpr_2.0,f_auto,q_auto,w_1400/fl_lossy,pg_1/nx8kyc4u7hwlrfkejotl/rw?fimg-ssr");
+//        Album album2 = new Album("Her Loss", "Drake & 21 Savage", 16, 60.33, "https://images.complex.com/complex/images/c_fill,dpr_2.0,f_auto,q_auto,w_1400/fl_lossy,pg_1/rrgyzeu7ucbf5styxkuw/drake-21-savage-her-loss-album-cver?fimg-ssr");
+//        Album album3 = new Album("An Evening with Silk Sonic", "Silk Sonic", 10, 31.19, "https://images.complex.com/complex/images/c_fill,dpr_2.0,f_auto,q_auto,w_1400/fl_lossy,pg_1/n1spfyib0hwmvx7xvu75/silk-sonic?fimg-ssr");
+//
+//        List<Album> albums = new ArrayList<>();
+//        albums.add(album1);
+//        albums.add(album2);
+//        albums.add(album3);
+        List<Album> albums = albumRepository.findAll();
 
-        // second, create an ArrayList to hold them
-        List<Album> albums = new ArrayList<>();
-        albums.add(album1);
-        albums.add(album2);
-        albums.add(album3);
-
-        // then send the albums to the view
-        // Using the Model, include key:value pairs
         m.addAttribute("albums", albums);
-        // return to the template of album
         return "albums.html";
     }
     // post route to stores
     @PostMapping("/albums")
-    // RedirectView
-    // Makes a GET request to the path given
-    // We are returning a RedirectView so that we can immediately retrieve and display the new store we just POSTed
-    // If you test with Postman, make sure to do a POST request with x-www-from-urlencoded selected in Body
     public RedirectView addAlbum(String title,String artist, Integer songCount, Double length, String imgUrl ){
         // create new store
         Album newAlbum = new Album(title, artist, songCount, length, imgUrl);
         // add it to the DB
-        AlbumRepository.save(newAlbum);
+        albumRepository.save(newAlbum);
         return new RedirectView("/albums");
     }
 }
